e2b72ab9990bc0ada82b83d9fff072d3516399d7
mIraj <mirajmahmud@iut-dhaka.edu>
2025-04-13 21:32:30 +0600

    login works perfectly... made a demo page for login... also signup can create new users in the database

--- frontend/src/pages/auth/login/login.js
+++ frontend/src/pages/auth/login/login.js
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './login.css'; // CSS file
+import { useNavigate } from 'react-router-dom';
 
-const login = () => {
+const Login = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData]=useState({
+    email:'',
+    password: '',
+  })
+
+  const handleInputChange = (event) =>{
+    const {name, value} = event.target;
+    setFormData({
+      ...formData,
+      [name]:value
+    })
+  }
+
+  const handleSubmit = async(e) =>
+  {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3000/auth/login",{
+        method:"POST",
+        headers:{
+          "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(formData)
+      })
+      const result = await response.json();
+      console.log(result);
+      navigate("/dashboard");
+      
+    } catch (error) {
+      console.error(error.message);
+      
+    } finally{
+      setFormData({
+        email: "",
+        password:"",
+
+      })
+
+    }
+  }
+
   return (
-    <div>
-      Login
+    <div className="login-container">
+      <h2>Signup</h2>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <label>Email Address</label>
+        <input type="email" name="email" placeholder="Enter email" value={formData.email} onChange={handleInputChange} />
+
+        <label>Password</label>
+        <input type="password" name="password" placeholder="Enter password" value={formData.password} onChange={handleInputChange}/>
+
+        <button type="submit">Login</button>
+      </form>
     </div>
   )
 }
 
-export default login
+export default Login
